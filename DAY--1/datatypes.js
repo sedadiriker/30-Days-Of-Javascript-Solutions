6bfa0960c6f3c133c47874513e73b7b20763b267
Seda Diriker <146026945+sedadiriker@users.noreply.github.com>
2023-11-07 18:25:30 +0300

    add day--1

---
+++ DAY--1/datatypes.js
@@ -0,0 +1,10 @@
+
+// 5-Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+let firstName1 = 'Seda', // string
+    isLastNameDiriker1 = true, // boolean
+    age1 = null,     // null
+    birthYear1 = undefined;   // undefined
+
+console.log(typeof firstName1, isLastNameDiriker1, age1, birthYear1);
+
---
+++ DAY--1/variable.js
@@ -0,0 +1,62 @@
+// 4-Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let firstName = 'Seda', // string
+    isLastNameDiriker = true, // boolean
+    age = null,     // null
+    birthYear = undefined;   // undefined
+
+
+console.log(firstName, isLastNameDiriker, age, birthYear);
+
+
+
+// 6- Declare four variables without assigning values
+
+let variable1;
+let variable2;
+let variable3;
+let variable4;
+
+console.log(variable1); // undefined
+
+
+// 7- Declare four variables with assigned values
+
+let variable5 = 'value1';
+let variable6 = 'value2';
+let variable7 = 'value3';
+let variable8 = 'value4';
+
+
+// 8- Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let firstname = 'Seda',
+    lastname = 'Diriker',
+    isMaried = true,
+    country = 'Turkey',
+    agE = 35;
+
+
+// 9- Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let firstnaMe = 'Seda'; lastnaMe = 'Diriker'; isMaried = true; country = 'Turkey'; agE = 35;
+
+
+// 10- Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 35,
+    yourAge = 30;
+
+console.log(`I am ${myAge} years old.`)
+console.log(`You are ${yourAge} years old.`)
+
+
+
+
+
+
+
+
+
+
+
