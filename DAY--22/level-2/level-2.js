dd731b07546c0bb666f51d383370373821c0207a
Seda Diriker <146026945+sedadiriker@users.noreply.github.com>
2024-01-20 20:30:56 +0300

    some fixes

--- DAY--22/level-2/level-2.js
+++ DAY--22/level-2/level-2.js
@@ -1,22 +1,31 @@
 import {countries} from "./countries.js"
 
-const div = document.createElement('div')
-
-for(let i = 0; i < 6; i++) {
-    const span = document.createElement('span')
-
-    document.body.appendChild(div)
-    div.appendChild(span)
-    span.textContent = 'yazı'+ i
-    span.style.border = '1px solid black'
-    span.style.width = '4rem'
-    span.style.height = '4rem'
-    span.style.marginLeft = '2px'  
-    span.style.fontSize = '2rem'
+for(let i = 0; i < countries.length; i+=6) {
+    let division = document.createElement('div')
+    division.style.display = "flex"
+    division.style.justifyContent = "center"
+    
+    for(let j = 0; j < 6; j++) {
+        let pElement = document.createElement('p')
+        division.append(pElement)
+        pElement.textContent = countries[i + j]
+        pElement.style.border = "1px solid black"
+        pElement.style.width = "15rem"
+        pElement.style.height = "15rem"
+        pElement.style.padding = '1rem'
+        pElement.style.margin = "0.5rem"
+        pElement.style.fontSize = '2rem';
+        pElement.style.display = 'flex';
+        pElement.style.justifyContent = 'center';
+        pElement.style.alignItems = 'center';
+    }
+    document.body.appendChild(division)
 }
 
-div.style.border = '1px solid red'
-div.style.textAlign = 'center'
-div.style.height = '4rem' 
 
 
+let totalNumber = document.querySelector('#total-countries')
+totalNumber.textContent = `Total Number of countries : ${countries.length}`
+
+totalNumber.style.fontSize = '2rem'
+totalNumber.style.textAlign = 'center'
